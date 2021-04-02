65f03b1cbea703d9b434c55c8def3986cad0a079
Aaron Kurowski <aaron12342@att.net>
2021-04-02 13:47:20 -0500

    Cleaned up random destination/restaurant functions and added randomly selected transportation and entertainment functions. Now prompts user asking if they would like to plan a random day trip. Outputs destination, restaurant based on destination, mode of transportation, and entertainment.

--- DaytripGenerator.js
+++ DaytripGenerator.js
@@ -1,23 +1,65 @@
 "use strict";
-// This is the day trip generator
+// This is a day trip generator for southern wisconsinites
 
 let destinations = ["Milwaukee", "Chicago", "Kenosha", "Racine", "Burlington"];
 let kenoshaRestaurants = ["La Fogata", "Ian's Pizza", "Brat Stop", "The Coffee Pot", "Tommy's"];
-let milwaukeeRestaurants = [];
-let racineRestaurants = [];
-let burlingtonRestaurants = [];
-let modeOfTransport = ["Personal Vehicle", "Public transit", "Uber", "On-foot"];
-let entertainment = ["Frisbee Golf", "Cinema", "Bowling", ""];
+let milwaukeeRestaurants = ["La Merenda", "Odd Duck", "Harbor House", "Vanguard", "National Pizza Pub"];
+let racineRestaurants = ["Blue Bear", "Jus' Tips", "Olde Madrid", "Reefpoint Brew House", "The Maple Table"];
+let burlingtonRestaurants = ["Pine Street Cafe", "Lucky Star Pub & Eatery", "Charcoal Grill",
+                             "Napoli Restaurant & Pizzeria", "Burlington Tap and Smokehouse"];
+let chicagoRestaurants = ["Flour Power", "Sushi Suite 202", "The Capital Grill"];
+let modeOfTransport = ["Personal vehicle", "Public transit", "Uber", "On-foot", "Bicycle"];
+let entertainment = ["Frisbee Golf", "Cinema", "Bowling", "Beach walk", "Pub crawl"];
+let yourDayTrip = [];
 
-function generateRandomInt(){
-    return Math.floor(Math.random() * 6);
+let pickedDestination;
+let pickedRestaurant;
+let pickedModeOfTransport;
+let pickedEntertainment;
+
+function randomDestination(){
+    return destinations[Math.floor(Math.random() * 5)];
 }
-let randomNum = generateRandomInt();
-console.log(randomNum);
 
-function chooseDestination(){
-    return destinations[generateRandomInt()];
+function randomRestaurant(){
+    if(pickedDestination === "Milwaukee"){
+        return milwaukeeRestaurants[Math.floor(Math.random() * 5)]
+    }
+    else if(randomDestination === "Chicago"){
+        return chicagoRestaurants[Math.floor(Math.random() * 5)]
+        }
+    else if(randomDestination === "Kenosha"){
+        return kenoshaRestaurants[Math.floor(Math.random() * 5)]
+        }
+    else if(randomDestination === "Burlington"){
+        return burlingtonRestaurants[Math.floor(Math.random() * 5)]
+    }
+    else{
+        return racineRestaurants[Math.floor(Math.random() * 5)]
+    }
 }
 
-let randomRestaurant = chooseDestination();
-console.log(randomRestaurant);
+function randomModeOfTransport(){
+    return modeOfTransport[Math.floor(Math.random() * 5)];
+}
+
+function randomEntertainment(){
+    return entertainment[Math.floor(Math.random() * 5)];
+}
+
+let userInput = prompt("Would you like to randomly plan a day trip? (yes/no)");
+if( userInput.toLowerCase() === 'yes'){
+    pickedDestination = randomDestination();
+    pickedRestaurant = randomRestaurant();
+    pickedModeOfTransport = randomModeOfTransport();
+    pickedEntertainment = randomEntertainment();
+    console.log("Destination: " + pickedDestination);
+    console.log("Restaurant: " + pickedRestaurant);
+    console.log("Mode of transportation: " + pickedModeOfTransport);
+    console.log("Entertainment: " + pickedEntertainment);
+}
+else{
+    console.log("Okay, plan your own trip!")
+}
+
+// allow the user to change a part of the trip
