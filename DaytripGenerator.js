198fc2770f4a45eb75dddef0c4a1a3ab83e56e5b
Aaron Kurowski <aaron12342@att.net>
2021-04-04 21:31:56 -0500

    Now allows the user to more comfortably change aspects of the trip with just a number input. More restaurants added and prettier display messages with better line breaks.

--- DaytripGenerator.js
+++ DaytripGenerator.js
@@ -5,12 +5,12 @@
 let destinations = ["Milwaukee", "Chicago", "Kenosha", "Racine", "Burlington"];
 let kenoshaRestaurants = ["La Fogata", "Ian's Pizza", "Brat Stop", "The Coffee Pot", "Tommy's", "Honada", "Luigi's", 
                           "Sazzy B", "House of Gerhard"];
-let milwaukeeRestaurants = ["La Merenda", "Odd Duck", "Harbor House", "Vanguard", "National Pizza Pub"];
+let milwaukeeRestaurants = ["La Merenda", "Odd Duck", "Harbor House", "Vanguard", "National Pizza Pub", "Zarletti", ];
 let racineRestaurants = ["Blue Bear", "Jus' Tips", "Olde Madrid", "Reefpoint Brew House", "The Maple Table"];
 let burlingtonRestaurants = ["Pine Street Cafe", "Lucky Star Pub & Eatery", "Charcoal Grill",
-                             "Napoli Restaurant & Pizzeria", "Burlington Tap and Smokehouse"];
-let chicagoRestaurants = ["Flour Power", "Sushi Suite 202", "The Capital Grill", "Homeslice", "Taste of Chicago Restaurant"];
-let modeOfTransport = ["Personal vehicle", "Public transit", "Uber", "On-foot", "Bicycle"];
+                             "Napoli Restaurant & Pizzeria", "Burlington Tap and Smokehouse", "Lucky Star", "The Waterfront"];
+let chicagoRestaurants = ["Flour Power", "Sushi Suite 202", "The Capital Grill", "Homeslice", "Taste of Chicago Restaurant", "The House of Blues"];
+let modeOfTransport = ["Personal vehicle", "Public transit", "Uber", "On-foot", "Bicycle", "Helicopter"];
 let entertainment = ["Frisbee Golf", "Cinema", "Bowling", "Beach walk", "Pub crawl", "Geo-caching"];
 
 // variables for options generated
@@ -19,7 +19,7 @@ let pickedRestaurant;
 let pickedModeOfTransport;
 let pickedEntertainment;
 
-// generates random restaurant
+// generates random destination
 function randomDestination(){
     return destinations[Math.floor(Math.random() * destinations.length)];
 }
@@ -61,6 +61,7 @@ function displayTrip(){
     console.log("Entertainment: " + pickedEntertainment);
 }
 
+// removes a specific element from an array
 function removeArrayElement(array, value){
     let index = array.indexOf(value);
     if(index >= 0){
@@ -82,23 +83,24 @@ if(userInput.toLowerCase() === 'yes'){
     let tripImpending = true;
     while(tripImpending){
         let acceptancePrompt = prompt("Do you want this trip? (yes/no)")
-        acceptancePrompt.toLowerCase();
-        if(acceptancePrompt === 'yes'){
+        if(acceptancePrompt.toLowerCase() === 'yes'){
             tripImpending = false;
             console.log("Enjoy your trip!");
             break;
         }
+        else if(acceptancePrompt.toLowerCase() !== 'yes' && acceptancePrompt.toLowerCase() !== 'no'){
+            alert("Incorrect input");
+        }
         else{
-            let userChange = prompt("What part will you change? (destination/restaurant/transportation/entertainment)")
+            let userChange = prompt("What part will you change?" + "\n" + "Type the word or corresponding number to make changes." + 
+                                    "\n" + "1=destination, 2=restaurant, 3=transportation, 4=entertainment")
 
             // change destination
-            if(userChange.toLowerCase() === 'destination'){
+            if(userChange.toLowerCase() === 'destination' || userChange === '1'){
                 removeArrayElement(destinations, pickedDestination);
                 if(destinations.length === 0){
                     console.log("\n" + "We've run out of destination!");
                     break;
-                    //pickedDestination = "We've run out of destinations!";
-                    //displayTrip();
                 }
                 else{
                     pickedDestination = randomDestination();
@@ -109,7 +111,7 @@ if(userInput.toLowerCase() === 'yes'){
             }
 
             // change restaurant
-            else if(userChange.toLowerCase() === 'restaurant'){
+            else if(userChange.toLowerCase() === 'restaurant' || userChange === '2'){
                 if(pickedDestination === "Milwaukee"){
                     removeArrayElement(milwaukeeRestaurants, pickedRestaurant)
                     if(milwaukeeRestaurants.length === 0){
@@ -117,55 +119,59 @@ if(userInput.toLowerCase() === 'yes'){
                         displayTrip();
                     }
                     else{
-                    pickedRestaurant = randomRestaurant();
-                    displayTrip();
+                        pickedRestaurant = randomRestaurant();
+                        displayTrip();
                     }
                 }
                 else if(pickedDestination === "Kenosha"){
                     removeArrayElement(kenoshaRestaurants, pickedRestaurant);
                     if(kenoshaRestaurants.length === 0){
                         pickedRestaurant = "No Kenosha restaurants left";
+                        displayTrip();
                     }
                     else{
-                    pickedRestaurant = randomRestaurant();
-                    displayTrip();
+                        pickedRestaurant = randomRestaurant();
+                        displayTrip();
                     }
                 }
                 else if(pickedDestination === "Chicago"){
                     removeArrayElement(chicagoRestaurants, pickedRestaurant);
                     if(chicagoRestaurants.length === 0){
                         pickedRestaurant = "No Chicago restaurants left";
+                        displayTrip();
                     }
                     else{
-                    pickedRestaurant = randomRestaurant();
-                    displayTrip();
+                        pickedRestaurant = randomRestaurant();
+                        displayTrip();
                     }
                 }
                 else if(pickedDestination === "Burlington"){
                     removeArrayElement(burlingtonRestaurants, pickedRestaurant);
                     if(burlingtonRestaurants.length === 0){
                         pickedRestaurant = "No Burlington restaurants left";
+                        displayTrip();
                     }
                     else{
-                    pickedRestaurant = randomRestaurant();
-                    displayTrip();
+                        pickedRestaurant = randomRestaurant();
+                        displayTrip();
                     }
                 }
                 else{
                     removeArrayElement(racineRestaurants, pickedRestaurant);
                     if(racineRestaurants.length === 0){
                         pickedRestaurant = "No Racine restaurants left";
+                        displayTrip();
                     }
                     else{
-                    pickedRestaurant = randomRestaurant();
-                    displayTrip();
+                        pickedRestaurant = randomRestaurant();
+                        displayTrip();
                     }
                 }
                 
             }
 
             // change transportation
-            else if(userChange.toLowerCase() === 'transportation'){
+            else if(userChange.toLowerCase() === 'transportation' || userChange === '3'){
                 removeArrayElement(modeOfTransport, pickedModeOfTransport);
                 if(modeOfTransport.length === 0){
                     pickedModeOfTransport = "No modes of transportation left";
@@ -178,10 +184,10 @@ if(userInput.toLowerCase() === 'yes'){
             }
 
             // change entertainment
-            else if(userChange.toLowerCase() === 'entertainment'){
+            else if(userChange.toLowerCase() === 'entertainment' || userChange === '4'){
                 removeArrayElement(entertainment, pickedEntertainment);
                 if(entertainment.length === 0){
-                    pickedEntertainment = "No options left";
+                    pickedEntertainment = "No entertainment options left";
                     displayTrip();
                 }
                 else{
@@ -190,7 +196,7 @@ if(userInput.toLowerCase() === 'yes'){
                 }
             }
             else{
-                console.log("Type one of the options again");
+                alert("Incorrect input")
             }
         }
     }
--- ScratchWorkDTG.js
+++ ScratchWorkDTG.js
@@ -1,3 +1,6 @@
+"use strict";
+
+/*
 function removeArrayElement(array, value){
     let index = array.indexOf(value);
     if(index >= 0){
@@ -7,8 +10,59 @@ function removeArrayElement(array, value){
 }
 
 arrayEx = ["hi", "bye", "sup"];
-
 console.log(removeArrayElement(arrayEx, "hi"));
+*/
 
 
+// arrays for random selections
+let destinations = ["Milwaukee", "Chicago", "Kenosha", "Racine", "Burlington"];
+let kenoshaRestaurants = ["La Fogata", "Ian's Pizza", "Brat Stop", "The Coffee Pot", "Tommy's", "Honada", "Luigi's", 
+                          "Sazzy B", "House of Gerhard"];
+let milwaukeeRestaurants = ["La Merenda", "Odd Duck", "Harbor House", "Vanguard", "National Pizza Pub"];
+let racineRestaurants = ["Blue Bear", "Jus' Tips", "Olde Madrid", "Reefpoint Brew House", "The Maple Table"];
+let burlingtonRestaurants = ["Pine Street Cafe", "Lucky Star Pub & Eatery", "Charcoal Grill",
+                             "Napoli Restaurant & Pizzeria", "Burlington Tap and Smokehouse"];
+let chicagoRestaurants = ["Flour Power", "Sushi Suite 202", "The Capital Grill", "Homeslice", "Taste of Chicago Restaurant"];
+let modeOfTransport = ["Personal vehicle", "Public transit", "Uber", "On-foot", "Bicycle"];
+let entertainment = ["Frisbee Golf", "Cinema", "Bowling", "Beach walk", "Pub crawl", "Geo-caching"];
 
+// variables for options generated
+let pickedDestination;
+let pickedRestaurant;
+let pickedModeOfTransport;
+let pickedEntertainment;
+
+
+function createDayTrip(){
+    // get day trip plans
+    let dayTripPlans = [];
+    pickedDestination = destinations[Math.floor(Math.random) * destinations.length];
+    pickedRestaurant = kenoshaRestaurants[Math.floor(Math.random) * kenoshaRestaurants.length];
+    pickedModeOfTransport = modeOfTransport[Math.floor(Math.random) * modeOfTransport.length];
+    pickedEntertainment = entertainment[Math.floor(Math.random) * entertainment.length];
+    dayTripPlans.push(pickedDestination);
+    dayTripPlans.push(pickedRestaurant);
+    dayTripPlans.push(pickedModeOfTransport);
+    dayTripPlans.push(pickedEntertainment);
+    return dayTripPlans;
+            
+}
+
+let createdDayTrip = createDayTrip();
+console.log(createdDayTrip);
+
+function displayDayTrip(){
+    console.log(createdDayTrip[0]);
+    console.log(createdDayTrip[1]);
+    console.log(createdDayTrip[2]);
+    console.log(createdDayTrip[3]);
+}
+/*
+// generates random destination
+function randomDestination(){
+    pickedDestination = destinations[Math.floor(Math.random() * destinations.length)];
+    return pickedDestination;
+}
+let createdDestination = randomDestination();
+console.log(createdDestination);
+*/
